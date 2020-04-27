caa08169e5b1a60dc7fd3ff9e260c9a497340b3b
f0rGetFull <f0rgetful@yandex.ru>
2020-04-27 13:21:52 +0300

    begin

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@ import "./style.sass"
 //json вуз разрешения он его и так прнимает
 import json from "./assets/json"
 import logo from "./assets/1.png"
-import csv from "./assets/1"
-import svn from "./assets/1.xml"
+// import csv from "./assets/1"
+// import svn from "./assets/1.xml"
 import * as $ from 'jquery';
 import 'bootstrap';
 //Разные форматы
